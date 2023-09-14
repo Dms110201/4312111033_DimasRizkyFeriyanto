d8ad3be38fae82ef5f2317782075dbba8e9c37e2
Dms110201 <dimasrizkiferiyanto@gmail.com>
2023-09-14 22:41:19 +0700

    Icon

--- lib/main.dart
+++ lib/main.dart
@@ -23,15 +23,34 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pratikum3'),
+        title: const Text('Pratiku3'),
       ),
-          body: const Text('Ini Text', style: TextStyle(
-            color: Colors.blue,
-              backgroundColor: Colors.pink,
-                fontSize: 20.0,
-              fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold
-      ),)
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.access_alarm),
+                        Text('Alarm')
+                        ],
+                        ),
+                        Column(
+                        children: <Widget>[
+                        Icon(Icons.phone),
+                        Text('Phone')
+                        ],
+                        ),
+                      Column(
+                    children: <Widget>[
+                  Icon(Icons.book),
+                Text('Book')
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
