d4f5cc17279cc4d8b32663196c93c7b3c89169df
Dms110201 <dimasrizkiferiyanto@gmail.com>
2023-10-07 18:43:43 +0700

    Latihan Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const SnackBarDemo());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'SnackBar Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar Demo'),
+        ),
+        body: const SnackBarPage(),
       ),
-      home: const testtoats(),
     );
   }
 }
 
-class testtoats extends StatefulWidget {
-  const testtoats({super.key});
-
-  @override
-  State<testtoats> createState() => _testtoatsState();
-}
-
-class _testtoatsState extends State<testtoats> {
+class SnackBarPage extends StatelessWidget {
+  const SnackBarPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Toast"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              // Langkah 3
-              Fluttertoast.showToast(
-                msg: "THE toast message",
-                toastLength: Toast.LENGTH_SHORT,
-                timeInSecForIosWeb: 1,
-                backgroundColor: Colors.black,
-                textColor: Colors.white,
-                fontSize: 16.0,
-              );
-            },
-            child: const Text(
-              'Show Toast Message',
-              style: TextStyle(fontSize: 24),
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {},
             ),
-          ),
-        ],
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
       ),
     );
   }
